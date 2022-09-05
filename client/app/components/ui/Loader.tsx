ad15568428014c3e81080bf66cae6eb45502ccf7
IceDiver <admin@example.com>
2022-09-05 20:19:46 +0300

    add dark theme

--- client/app/components/ui/Loader.tsx
+++ client/app/components/ui/Loader.tsx
@@ -1,17 +1,18 @@
-import {FC} from 'react'
-import Skeleton, {SkeletonProps} from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import { FC } from 'react';
+import Skeleton, { SkeletonProps } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
+import { useTheme } from '@/hooks/useTheme';
 
 const Loader: FC<SkeletonProps> = props => {
-	
+	const { isDarkTheme } = useTheme();
 	return (
 		<Skeleton
-			baseColor={'#D7E3FF'}
-			highlightColor={'#E8EDFB'}
+			baseColor={isDarkTheme ? '#424451' : '#d7e3ff'}
+			highlightColor={isDarkTheme ? '#636571' : '#e8edfb'}
 			{...props}
 		/>
-	)
-}
+	);
+};
 
-export default Loader
+export default Loader;
